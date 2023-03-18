273b341990aa31fa2451c43bc2b13800bdbfb050
Yosef Tawil <yoseftawil@gmail.com>
2023-03-18 18:08:54 +0200

    Functional prototype, need to add feature for minimum assignments weight, and fix some error checking with empty fields

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   const [finalGrade, setFinalGrade] = useState();
   const [errorMsg, setErrorMsg] = useState("");
+  const [calcMethod, setCalcMethod] = useState("radio-best");
   const [testGrade, setTestGrade] = useState();
   const [assignments, setAssignments] = useState([]);
 
@@ -21,12 +22,15 @@ function App() {
 
   useEffect(() => {
     checkErrors();
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[testGrade, assignments]);
+  },[testGrade, assignments, calcMethod]);
 
   function updateTestGrade(e){
     setTestGrade(e.target.value);
+  }
+
+  function settMethod(e){
+    setCalcMethod(e.target.id);
   }
 
   function handleChange(e) {
@@ -44,7 +48,7 @@ function App() {
   }
 
   function checkErrors() {
-    {/* Check test grade */}
+    /* Check test grade */
     const testGradeNum = Number(testGrade);
     let msg = "";
     if(notValid(testGradeNum)){
@@ -68,16 +72,33 @@ function App() {
         break;
       }
     }
-    
     setErrorMsg(msg);
+    calculateGrade();
   }
 
   function notValid(x){
-    return isNaN(x) || !Number.isInteger(x) || x < 0 || x > 100;
+    return isNaN(x) || !Number.isInteger(x) || x < 0 || x > 100 || x === "";
   }
   
   function calculateGrade() {
-
+    let totalWeight = 0, totalPoints = 0;
+    
+    for(let i = 0; i < assignments.length; i++){
+      let curr = assignments[i];
+      if(calcMethod === "radio-best"){
+        if(Number(curr.grade) <= testGrade){
+          continue;
+        }
+      }
+      let currWeight = Number(curr.weight);
+      totalWeight += currWeight
+      totalPoints += (currWeight / 100) * Number(curr.grade);
+      setFinalGrade(-1 * Math.round(-1 * totalPoints));
+    }
+    console.log("Wight: " + totalWeight);
+    console.log("Points: " + totalPoints);
+    totalPoints += ((100 - totalWeight) / 100) * testGrade;
+    setFinalGrade(-Math.round(-totalPoints));
   }
   
   function addAssignment() {
@@ -124,6 +145,7 @@ function App() {
           {/* FINAL GRADE DISPLAY */}
           <div className="text-center">
           <label><b>Final Grade: </b>{finalGrade}</label>
+          <br/>
           <label>{errorMsg}</label>
           </div>
 
@@ -132,17 +154,19 @@ function App() {
             <div className="mb-3">
               <Form.Check
                 label="Use Best Assignments"
-                name="group1"
+                name="calcMethod"
                 type="radio"
-                id={`inline-radio-1`}
+                id={`radio-best`}
                 defaultChecked
+                onChange={settMethod}
               />
               
               <Form.Check
                 label="Use All Assignments"
-                name="group1"
+                name="calcMethod"
                 type="radio"
-                id={`inline-radio-2`}
+                id={`radio-all`}
+                onChange={settMethod}
               />
             </div>
           </Form>
